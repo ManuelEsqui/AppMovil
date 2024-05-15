32ddda0dbdd4c20e1cb4a5a05de1545f6f49181f
6003099 <manuel.esquivel@viewnext.com>
2024-05-15 18:02:31 +0200

    primera pantalla

--- app/src/main/java/com/example/prueba/LogIn.java
+++ app/src/main/java/com/example/prueba/LogIn.java
@@ -1,6 +1,8 @@
 package com.example.prueba;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class LogIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_view);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+
+    public void registrarUsuario(View view) {
+        TextView prueba= findViewById(R.id.textView);
+        prueba.setText("Funciona xddd");
+    }
+
+    public void editarCuenta(View view) {
+    }
+
+    public void logIn(View view) {
     }
 }
